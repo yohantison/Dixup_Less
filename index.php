83d4f12a6507c73effd92e19a7ba6fdd7f7f2d68
yohantison <contact@yohantison.com>
2013-02-01 14:11:35 +0100

    Create Basic Structure

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+	<head>
+	    <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+		<title></title>
+		<link rel='shortcut icon' href=''>
+		<link rel='stylesheet' href='webroot/style/css/style.css' media='screen'/>
+	</head>
+
+	<body>
+		<div class="contentWrapper">
+
+			<header><h1>Pop in en Html 5</h1></header>
+
+			<nav></nav>
+
+			<section class="button">
+				<a href="#" class="popin">Cliquer pour ouvrir une popin</a>
+				<div class="popinContent">
+					<p>test</p>
+				</div>
+			</section>
+
+			<footer></footer>
+
+		</div>
+	</body>
+
+</html>
